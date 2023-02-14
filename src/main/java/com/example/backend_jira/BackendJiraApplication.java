aacaa5b3ab11f08def0fac144c8b8852e6044dc2
mura0717 <mura0717@stud.kea.dk>
2023-02-14 11:14:17 +0100

    Initial commit

---
+++ src/main/java/com/example/backend_jira/BackendJiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJiraApplication.class, args);
+    }
+
+}
